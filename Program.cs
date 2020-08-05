ac34c8ed97ec8ed5e80d4bea7153d85a5ca1ea03
Snuggle <^-^@Snugg.ie>
2020-08-05 14:48:56 +0100

    Asking some simple addition questions!
    Just simple print and read. Closes #1.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,14 @@ namespace MathsQuiz
     {
         static void Main(string[] args)
         {
+            Console.Write("What is 2 + 2?");
+            Console.ReadLine();
+            Console.Write("What is 1 + 9?");
+            Console.ReadLine();
+            Console.Write("What is 2 + 5?");
+            Console.ReadLine();
+            Console.Write("What is 2 + 3?");
+            Console.ReadLine();
         }
     }
 }
