4f2bbf273adb4eb9164bf2a159d0f1406e93dd44
Snuggle <^-^@Snugg.ie>
2020-08-05 17:10:17 +0100

    Added randomness! (Oops!)
    Closes #4 and #5.

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace MathsQuiz
 {
     class Program
     {
+        Random rnd = new Random();
+
         static string ListToString(List<bool> someList)
         {
             return String.Join(",", someList);
@@ -15,19 +17,43 @@ namespace MathsQuiz
 
         static void Main(string[] args)
         {
-            List<bool> results = new List<bool>();
-                     
+            List<char> operators = new List<char>() { '+', '-', '*', '/' };
+            int maxRandNumber = 100;
+
             Program P = new Program();
-            results.Add(P.AskQuestion('+', 5, 2));
-            results.Add(P.AskQuestion('-', 5, 2));
-            results.Add(P.AskQuestion('*', 5, 2));
-            results.Add(P.AskQuestion('/', 10, 2));
+            int numberOfQuestions = P.HowManyQuestions();
+
+            List<bool> results = new List<bool>();
+            for (int questionNum = 1; questionNum <= numberOfQuestions; questionNum++)
+            {
+                char op = operators[P.rnd.Next(0, operators.Count())];
+                int firstNum = P.rnd.Next(0, maxRandNumber);
+                int secondNum = P.rnd.Next(0, maxRandNumber);
+                results.Add(P.AskQuestion(op, firstNum, secondNum));
+            }
 
             Func<bool, bool> isTrue = x => x;
             int correctCount = results.Count(isTrue);
 
             Console.WriteLine("\nYou have gotten '" + correctCount + "' out of '" + results.Count + "' answers correct!");
             Console.ReadLine(); // Don't quit instantly.
+        }
+
+        public int HowManyQuestions()
+        {
+            Console.Write("How many questions would you like to attempt? ");
+            bool answerIsInt = Int32.TryParse(Console.ReadLine(), out int answer);
+            if (answerIsInt)
+            {
+                return answer;
+            }
+            else
+            {
+                // Input was not valid, try again.
+                Console.WriteLine("Please type in an integer and press ENTER. Repeating question...");
+                return HowManyQuestions();
+            }
+
         }
 
         public bool AskQuestion(char op, int firstNum, int secondNum)
@@ -39,12 +65,14 @@ namespace MathsQuiz
             // Receive answer, must be an integer.
             bool answerIsInt = Int32.TryParse(Console.ReadLine(), out int answer);
             if (answerIsInt)
-            {  
+            {
                 // Check if answer is correct or not!
                 bool correct = CheckAnswer(op, firstNum, secondNum, answer);
-                if (correct) {
+                if (correct)
+                {
                     Console.WriteLine("You are correct!");
-                } else
+                }
+                else
                 {
                     Console.WriteLine("Not correct!");
                 }
@@ -53,11 +81,11 @@ namespace MathsQuiz
             else
             {
                 // Input was not valid, try again.
-                Console.WriteLine("Please type in a number and press ENTER. Repeating question...")
+                Console.WriteLine("Please type in an integer and press ENTER. Repeating question...");
                 return AskQuestion(op, firstNum, secondNum);
             }
         }
-        
+
         public bool CheckAnswer(char op, int firstNum, int secondNum, int answer)
         {
             switch (op)
