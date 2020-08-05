d19df0cde73a32c022063b88b431adbe87e1b30f
Snuggle <^-^@Snugg.ie>
2020-08-05 16:32:51 +0100

    Added feedback for the user's answers!
    Closes #2. Might contain slightly messy code, I had a big headache while doing this section!

--- Program.cs
+++ Program.cs
@@ -8,16 +8,66 @@ namespace MathsQuiz
 {
     class Program
     {
+        static string ListToString(List<bool> someList)
+        {
+            return String.Join(",", someList);
+        }
+
         static void Main(string[] args)
         {
-            Console.Write("What is 2 + 2?");
-            Console.ReadLine();
-            Console.Write("What is 1 + 9?");
-            Console.ReadLine();
-            Console.Write("What is 2 + 5?");
-            Console.ReadLine();
-            Console.Write("What is 2 + 3?");
-            Console.ReadLine();
+            List<bool> results = new List<bool>();
+                     
+            Program P = new Program();
+            results.Add(P.AskQuestion('+', 2, 3));
+            results.Add(P.AskQuestion('+', 3, 4));
+            results.Add(P.AskQuestion('+', 4, 5));
+            results.Add(P.AskQuestion('+', 5, 6));
+
+            Func<bool, bool> isTrue = x => x;
+            int correctCount = results.Count(isTrue);
+
+            Console.WriteLine("\nYou have gotten '" + correctCount + "' out of '" + results.Count + "' answers correct!");
+            Console.ReadLine(); // Don't quit instantly.
+        }
+
+        public bool AskQuestion(char op, int firstNum, int secondNum)
+        {
+            // Ask question!
+            String s = String.Format("What is the answer to {0} {1} {2}? ", firstNum, op, secondNum);
+            Console.Write(s);
+
+            // Receive answer, must be an integer.
+            bool answerIsInt = Int32.TryParse(Console.ReadLine(), out int answer);
+            if (answerIsInt)
+            {  
+                // Check if answer is correct or not!
+                bool correct = CheckAnswer(op, firstNum, secondNum, answer);
+                if (correct) {
+                    Console.WriteLine("You are correct!");
+                } else
+                {
+                    Console.WriteLine("Not correct!");
+                }
+                return correct;
+            }
+            else
+            {
+                // Input was not valid, try again.
+                return AskQuestion(op, firstNum, secondNum);
+            }
+        }
+        
+        public bool CheckAnswer(char op, int firstNum, int secondNum, int answer)
+        {
+            switch (op)
+            {
+                case '+':
+                    return (firstNum + secondNum) == answer;
+                case '-':
+                    return (firstNum - secondNum) == answer;
+                default:
+                    return false;
+            }
         }
     }
 }
